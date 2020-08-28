379759ad9b381b927f4f5dd1a01c9f291200f491
Ayush Gupta <721ayush@gmail.com>
2020-08-28 08:26:32 +0530

    first commit

---
+++ src/CommentDetail.js
@@ -0,0 +1,27 @@
+/*
+ *   Copyright (c) 2020
+ *   All rights reserved.
+ */
+import React from "react";
+import faker from "faker";
+
+const CommentDetail = (props) => {
+  return (
+    <div className="comment">
+      <a href="/" className="avatar">
+        <img alt="avatar" src={faker.image.avatar()} />
+      </a>
+      <div className="content">
+        <a href="/" className="author">
+          {props.author}
+        </a>
+        <div className="metadata">
+          <span className="date">Posted on {props.date}</span>
+        </div>
+        <div className="text">Nice Blog Post</div>
+      </div>
+    </div>
+  );
+};
+
+export default CommentDetail;
